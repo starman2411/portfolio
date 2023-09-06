e4d60559e58347c57413a87ddfc51aaa9ba460ff
Nikita_Bakery <nikitaf71@gmail.com>
2023-09-06 21:57:05 +0300

    finishing title

--- js/script.js
+++ js/script.js
@@ -6,3 +6,13 @@ ScrollSmoother.create({
 	wrapper: '.wrapper',
 	content: '.content'
 })
+
+
+$(document).ready(function(){
+    setTimeout(()=>{
+    let title1 = document.getElementsByClassName("typed-out-first")[0];
+    let title2 = document.getElementsByClassName("typed-out-second")[0];
+    title1.classList.add('animation-title-1');
+    title2.classList.add('animation-title-2');
+}, 300);
+});
